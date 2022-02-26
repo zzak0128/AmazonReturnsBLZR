0e3b6a2290dbed264e31e0d1687eb3705106064e
Zachary Zak <Zach.Zak@devzapps.com>
2022-02-25 21:23:39 -0600

    Updates to home widgets

---
+++ AmazonReturnsInventoryUI/Components/OrderInProgress.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using AmazonReturnsInventoryLibrary.Orders;
+using Microsoft.AspNetCore.Components;
+
+namespace AmazonReturnsInventoryUI.Components
+{
+    public partial class OrderInProgress
+    {
+        [Parameter]
+        public List<Order> Orders { get; set; }
+
+        List<Order> inProcessOrders = new List<Order>();
+
+        protected override void OnParametersSet()
+        {
+            GetOrdersInProgress();
+        }
+
+        private void GetOrdersInProgress()
+        {
+            inProcessOrders = Orders.FindAll(o => o.Status == OrderStatus.InProcess);
+            StateHasChanged();
+        }
+    }
+}
